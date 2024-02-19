268c5ed756ced1474a308e4db515fdb561027107
Victor Rios Saucedo <36146644+Rotooo@users.noreply.github.com>
2024-02-19 00:32:00 -0600

    19-02-2024
    Project Base

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HolaMundo = () => {
+  return <h1>Hola mundo</h1>;
+};
+
+export default HolaMundo;
---
+++ src/services/i18n.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function i18n() {
+  return (
+    <div>i18n</div>
+  )
+}
